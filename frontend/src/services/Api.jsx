a598100e8d49d84de8816d30c0eb70a6c54f9b81
neeraj-code-beep <neerajmishra757944@gmail.com>
2026-05-19 10:47:29 +0530

    small changes

--- frontend/src/services/Api.jsx
+++ frontend/src/services/Api.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 });
+
+console.log(import.meta.env.VITE_API_URL);
 
 export default API;
